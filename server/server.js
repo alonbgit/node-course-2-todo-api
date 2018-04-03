d89b5a4f2d23e8655a460f9bea8efa1b8175299c
RSPACE150\alon <alon.b@appnext.com>
2018-04-03 18:22:49 +0300

    changes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const { User } = require ('./models/user');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -55,6 +57,27 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.delete('/todos/:id', (req, res) => {
+  // get the id
+  // validate the id. if it not valid, return 404
+  // remove todo by id.
+  //     success - if no doc, send 404. if doc, send doc back with 200
+  //     error - return 400 with empty body
+
+  var id = req.params.id;
+  if (!ObjectID.isValid(id))
+    return res.status(404).send();
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo)
+      return res.status(404).send();
+    res.send(todo);
+  }).catch((err) => {
+    res.status(400).send();
+  });
+
+});
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
