b55cf1d7e8b53385cc6b5a99dee77d2ab38d761c
RSPACE150\alon <alon.b@appnext.com>
2018-04-04 15:07:40 +0300

    changes

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -59,6 +60,24 @@ UserSchema.methods.toJSON = function() {
   var userObject = user.toObject();
   return _.pick(userObject, ['_id', 'email']);
 }
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+
+  }
+  else {
+    next();
+  }
+});
 
 UserSchema.statics.findByToken = function(token) {
 
