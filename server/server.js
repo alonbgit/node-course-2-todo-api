f9fa242502f198f6f27906344be9fca49f691aee
RSPACE150\alon <alon.b@appnext.com>
2018-04-04 12:40:30 +0300

    changes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+var env = process.env.NODE_ENV;
+
 var express = require('express');
 var bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
 const { ObjectID } = require('mongodb');
@@ -74,6 +77,59 @@ app.delete('/todos/:id', (req, res) => {
     res.send(todo);
   }).catch((err) => {
     res.status(400).send();
+  });
+
+});
+
+app.patch('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id))
+    return res.status(404).send();
+
+  if (_.isBoolean(body.completed)) {
+    body.completedAt = new Date().getTime();
+  }
+  else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: {
+      body
+    }
+  }, {
+    new: true
+  }).then((todo) => {
+    if (!todo)
+      return res.status(404).send();
+    res.send(todo);
+  }).catch((err) => {
+    res.status(400).send();
+  });
+
+});
+
+// POST /users
+
+app.post('/users', (req, res) => {
+
+  var body = req.body;
+
+  var user = new User({
+    email: body.email,
+    password: body.password
+  });
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((err) => {
+    res.status(400).send(err);
   });
 
 });
