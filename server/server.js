2a9a0fe2ea07d50f40947ecf5280aaafe3742312
RSPACE150\alon <alon.b@appnext.com>
2018-04-04 17:48:18 +0300

    changes

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const { ObjectID } = require('mongodb');
 const { Todo } = require('./models/todo');
 const { User } = require ('./models/user');
 const { authenticate } = require('./middleware/authenticate');
+const bcrypt = require('bcryptjs');
 
 var app = express();
 
@@ -135,7 +136,20 @@ app.post('/users', (req, res) => {
 
 });
 
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
 
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user); 
+    });
+  }).catch((err) => {
+    res.status(400).send();
+  });
+
+});
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
